bcb84e488b1a2645c77d746b4f9452073a13fba3
sourav <souravghosh1500@gmnail.com>
2021-03-10 11:26:28 +0530

    initila commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
+import Header from './components/Header'
 
 function App() {
+  const [height,setHeight]=useState(0)
+  const [weight,setWeight]=useState(0)
+  const [answer,setAnswer]=useState(0)
+
+
+
+  const calculate=()=>{
+  setAnswer((weight/(height))*100);
+
+
+
+  }
+
+  const body=()=>{
+    return (
+      <>
+      <div style={{height:"100px",width:"200px",marginTop:"100px",marginLeft:"550px",justifyContent:"center",alignItems:"center"}}>
+        <label>Enter Your Height In CM:</label>
+        <br/>
+        <input type="number" value={height} onChange={(e)=>{setHeight(e.target.value)}}/>
+        <br/>
+        <label>Enter Your Weight In kg:</label>
+        <input type="number" value={weight} onChange={(e)=>{setWeight(e.target.value)}}/>
+        <br/>
+        <br/>
+        <button className="btn btn-primary" onClick={calculate}>Submit</button>
+
+        
+
+        
+
+      </div>
+
+
+
+      </>
+    )
+  }
+  const message=()=>{
+    return (
+    <>
+    {
+      (answer!=0)?
+      <>
+    <div >
+      <p style={{marginTop:"100px"}}>{`your BMI is ${answer} your suggested range is between 50-67 You are in healthy weight range` }</p>
+
+      </div>
+      
+      </>
+      :
+      null
+    }
+    </>
+    )
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      {body()}
+      
+      {message()}
+      
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div>
+            <nav class="navbar" style={{backgroundColor:"#66d9ff",color:'white'}}>
+                
+                <h3 style={{paddingLeft:"520px"}}>BMI Calculator</h3>
+                
+        
+</nav>
+        </div>
+    )
+}
+
+export default Header
